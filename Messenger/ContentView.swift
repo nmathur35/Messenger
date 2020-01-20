1883d8124ae0c54d4c17ca4ad246a975155b9362
guruduttperi <thegdaproject@gmail.com>
2020-01-20 12:23:41 -0500

    First

--- Messenger/ContentView.swift
+++ Messenger/ContentView.swift
@@ -9,13 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name = ""
+    
     var body: some View {
-        Text("Hello, World!")
+       // Text("Hello, World!")
+        NavigationView{
+            ZStack{
+                Color.blue
+                VStack{
+                    TextField("Name", text: $name).textFieldStyle(RoundedBorderTextFieldStyle()).padding()
+                    
+                    NavigationLink(destination: MsgPage(name:self.name)){
+                        HStack{
+                            Text("Join")
+                        }
+                        }.frame(width: 120, height: 120).background(Color.blue).foregroundColor(Color.white).cornerRadius(20)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MsgPage: View{
+    
+    var name = ""
+    var body : some View{
+        Text("hello")
     }
 }
