4d7b4dd2ce8d1b04084ce6b90d901e2e7221c168
guruduttperi <thegdaproject@gmail.com>
2020-01-20 18:11:56 -0500

    Added Chat UI

--- Messenger/ContentView.swift
+++ Messenger/ContentView.swift
@@ -41,14 +41,57 @@ struct ContentView_Previews: PreviewProvider {
 struct MsgPage: View{
     
     var name = ""
+    
+    @ObservedObject var msg = observer()
+    @State var typedmsg = ""
+    
     var body : some View{
-        Text(name).navigationBarTitle("Chats", displayMode: .large)
+        //Text(name).navigationBarTitle("Chats", displayMode: .large)
+        VStack{
+        List(msg.msgs){ i in
+            Text(i.msg)
+        }.navigationBarTitle("Chats", displayMode: .large)
+        
+            HStack{
+                
+                TextField("Msg", text: $typedmsg  ).textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    
+                }){
+                    Text("Send")
+                }
+            }.padding()
+        }
+        
     }
 }
 
 class observer: ObservableObject{
 
     @Published var msgs = [datatype]()
+    init() {
+        let db = Firestore.firestore()
+        db.collection("msgs").addSnapshotListener{
+            (snap, err)  in
+            
+            if err != nil{
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            for i in snap!.documentChanges{
+                if i.type == .added{
+                    let name = i.document.get("name") as! String
+                    let msg = i.document.get("msg") as! String
+                    let id = i.document.documentID
+                    
+                    self.msgs.append(datatype(id: id, name: name, msg: msg))
+                    
+                }
+            }
+        }
+    }
     
 }
 
