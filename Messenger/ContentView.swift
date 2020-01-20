867f95195e350ab2a65886bb2fcf7ab2f22ad4bb
guruduttperi <thegdaproject@gmail.com>
2020-01-20 11:51:20 -0500

    Initial Commit

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Gurudutt on 1/20/20.
+//  Copyright © 2020 Gurudutt Perichetla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
